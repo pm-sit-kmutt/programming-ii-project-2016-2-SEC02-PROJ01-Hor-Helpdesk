080f05ad52c67123a336d4735a984c79b6e25585
basskt02 <basza604@hotmail.com>
2017-04-08 13:09:45 +0700

    add Announce.jsp

--- HelpDesk/src/java/Servlet/AnnounceServlet.java
+++ HelpDesk/src/java/Servlet/AnnounceServlet.java
@@ -5,6 +5,7 @@
  */
 package Servlet;
 
+import Model.Announce;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -17,31 +18,18 @@ import javax.servlet.http.HttpServletResponse;
  * @author l3as
  */
 public class AnnounceServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AnnounceServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AnnounceServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        Announce Announce = Model.Announce.getAnnounce(1);
+        String target = "/Announce.jsp";
+        
+        request.setAttribute("message", Announce);
+        
+        getServletContext().getRequestDispatcher(target).forward(request, response);
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
