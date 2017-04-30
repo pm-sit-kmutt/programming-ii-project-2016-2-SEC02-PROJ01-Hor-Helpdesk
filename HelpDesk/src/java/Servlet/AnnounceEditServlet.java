0af6aa9fe82f253f2d6adedbda7eaca23648ec22
Boat <boat46450@hotmail.com>
2017-04-30 21:06:04 +0700

    edit bug thai language

--- HelpDesk/src/java/Servlet/AnnounceEditServlet.java
+++ HelpDesk/src/java/Servlet/AnnounceEditServlet.java
@@ -34,6 +34,7 @@ public class AnnounceEditServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         
         String title = request.getParameter("title");
--- HelpDesk/src/java/Servlet/AnnounceFormServlet.java
+++ HelpDesk/src/java/Servlet/AnnounceFormServlet.java
@@ -28,6 +28,7 @@ public class AnnounceFormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
         
         String title = request.getParameter("title");
